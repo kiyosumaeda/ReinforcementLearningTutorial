c5f4fa9b084d785f6beb13f381fd1f29020f20d0
kiyosu.maeda <kiyosu.0407@gmail.com>
2020-01-15 20:58:49 +0900

    add mc method on random walk

--- RLbook/RandomWalk/random_walk_analytics.py
+++ RLbook/RandomWalk/random_walk_analytics.py
@@ -3,15 +3,23 @@ import matplotlib.pyplot as plt
 def read_rms(l, filename):
 	with open(filename, "r") as f:
 		for row in f:
-			l.append(float(row))
+			l.append(float(row)/100)
 
 rms_01 = []
 rms_015 = []
 rms_005 = []
+rms_mc_01 = []
+rms_mc_02 = []
+rms_mc_03 = []
+rms_mc_04 = []
 
 read_rms(rms_01, "rms_0_1.txt")
 read_rms(rms_015, "rms_0_15.txt")
 read_rms(rms_005, "rms_0_05.txt")
+read_rms(rms_mc_01, "rms_mc_0_01.txt")
+read_rms(rms_mc_02, "rms_mc_0_02.txt")
+read_rms(rms_mc_03, "rms_mc_0_03.txt")
+read_rms(rms_mc_04, "rms_mc_0_04.txt")
 
 plt.figure(figsize=(12, 6))
 plt.title("Empiriacal RMS error, averaged over states")
@@ -19,4 +27,8 @@ plt.xlabel("Walks/Episodes")
 plt.plot(rms_01, color="blue", label="α=.1")
 plt.plot(rms_015, color="blue", label="α=.15")
 plt.plot(rms_005, color="blue", label="α=.05")
+plt.plot(rms_mc_01, color="red", label="α=.01")
+plt.plot(rms_mc_02, color="red", label="α=.02")
+plt.plot(rms_mc_03, color="red", label="α=.03")
+plt.plot(rms_mc_04, color="red", label="α=.04")
 plt.show()
--- RLbook/RandomWalk/random_walk_mc.py
+++ RLbook/RandomWalk/random_walk_mc.py
@@ -1,9 +1,8 @@
 import random
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 
-alpha = 0.05
+alpha = 0.04
 tv = np.arange(0, 1.1, 1/6)
 tv[6] = 0
 a = [-1, 1]
@@ -17,21 +16,27 @@ for j in range(run):
 	v = np.full(7, 0.5)
 	v[0] = 0
 	v[6] = 0
+	returns = [[0 for k in range(1)] for l in range(7)]
 	for i in range(episodes):
 		s = 3
-		while s>=1 and s<=5:
-			action_index = random.randint(0, 1);
+		state_list = []
+		state_list.append(s)
+		g = 0
+		while s>= 1 and s<=5:
+			action_index = random.randint(0, 1)
 			action = a[action_index]
 			new_state = s + action
-			reward = 0
+			state_list.append(new_state)
 			if new_state == 6:
-				reward = 1
-			v[s] = v[s] + alpha*(reward + gamma*v[new_state] - v[s])
+				g = 1
 			s = new_state
+		for m in range(len(state_list)):
+			if (state_list[m] != 6):
+				v[state_list[m]] = v[state_list[m]] + alpha*(g-v[state_list[m]])
 		rms[i] += (math.sqrt(np.sum((v-tv)**2)/5))
+		# print(rms[i])
+		# print(v)
 
-# print(rms)
-
-with open("rms_0_05.txt", "wt") as f:
+with open("rms_mc_0_04.txt", "wt") as f:
 	for e in rms:
 		f.write(str(e)+'\n')
